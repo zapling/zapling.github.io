5d1606be404c25a08df4ff28e669562a22fd27b4
Andreas Palm <andreaspalm98@gmail.com>
2020-10-17 16:13:05 +0200

    Hello world

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,17 @@
-console.log('Test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Website extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Website />,
+    document.getElementById('root')
+);
