b7757fe44c6e10ed66c9f0a792b420992627fd92
Andreas Palm <andreaspalm98@gmail.com>
2020-10-17 17:52:48 +0200

    Typer stuff

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typist from 'react-typist';
 
-class Website extends React.Component {
+class BootSequence extends React.Component {
     render() {
         return (
-            <div>
-                Hello World
-            </div>
+            <Typist
+                cursor={this.props.cursor}
+                delayGenerator={() => 1 }
+                onTypingDone={this.props.onDone}
+            >
+                Boot sequence
+                <ul>
+                    <li>Love for tacos</li>
+                    <li>Red bull junkie</li>
+                    <li>Snacksman</li>
+                    <li>Love for dogs and cats</li>
+                    <li>Speghetti code</li>
+                </ul>
+                <Typist.Delay ms={500} />
+            </Typist>
+        );
+    }
+}
+
+class Terminal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            sequence: 0,
+            cursor: {
+                show: false
+            }
+        }
+    }
+
+    nextSequence() {
+        this.setState({sequence: 1});
+    }
+
+    render() {
+        if (this.state.sequence === 0) {
+            return <BootSequence cursor={this.state.cursor} onDone={() => this.nextSequence()} />
+        }
+
+        return (
+            <div>We should type the ssh part now</div>
         );
     }
 }
 
 ReactDOM.render(
-    <Website />,
+    <Terminal />,
     document.getElementById('root')
 );
