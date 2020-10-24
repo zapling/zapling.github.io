e3e327047fe46cb6e347e58c355c47b5a1532c75
Andreas Palm <andreaspalm98@gmail.com>
2020-10-24 15:20:08 +0200

    First live push!

---
+++ docs/precache-manifest.ef8db5883639b2774d51b9356ef50e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd59f1c2fbff2fcea7d817bce996e349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0091a7e827ad906a2fc9",
+    "url": "/static/css/main.b680d1ce.chunk.css"
+  },
+  {
+    "revision": "7b543b45ee37770bb7ec",
+    "url": "/static/js/2.7e8deae9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7e8deae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0091a7e827ad906a2fc9",
+    "url": "/static/js/main.51f61519.chunk.js"
+  },
+  {
+    "revision": "31b0eb131cc5e984aeed",
+    "url": "/static/js/runtime-main.8aed62af.js"
+  }
+]);
